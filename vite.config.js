851fcbca39d823085e6989dac09e8a040689f23a
Akarsh Khandelwal <akarsh.khandelwal@onmeridian.com>
2026-02-11 11:48:55 +0530

    t

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
 import { pdfManifestPlugin } from './frontend/src/plugins/pdfManifestPlugin.js';
-
+//ls
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
